34dc38666bef2268df6b9187b6d427ded0d2e278
Akusem <nathanael.z@outlook.fr>
2020-10-27 18:38:45 +0100

    Add different language support

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.setInterval(() => {
   if (!dlButtonAlreadyPresent() && moreThanOneDlLink() && allLinksLoaded()) {
+    // if (moreThanOneDlLink() && allLinksLoaded()) {
     retrieveLinksAndAddDlButton();
   }
 }, 1000);
@@ -38,6 +39,7 @@ function retrieveLinksAndAddDlButton() {
 }
 
 function getAllDlLinks() {
+  const lang = getPageLanguage();
   let toDl = [];
 
   const container = document.getElementById("links-container");
@@ -45,13 +47,57 @@ function getAllDlLinks() {
     for (let possibleDlTag of individualLinkDiv.children) {
       if (
         possibleDlTag.tagName === "A" &&
-        possibleDlTag.textContent.startsWith("TELECHARGER")
+        startWithDl(possibleDlTag.textContent, lang)
       ) {
         toDl.push(possibleDlTag.href);
       }
     }
   }
   return toDl;
+}
+
+function getPageLanguage() {
+  const lang_list = document.getElementsByClassName("lang_list");
+  // Return trimed text contain in div.lang_list>a>strong
+  const language = lang_list[0].children[0].children[0].textContent.trim();
+  console.log(language);
+  return language;
+}
+
+function startWithDl(text, lang) {
+  switch (lang) {
+    case "Français (France)":
+      return text.startsWith("TELECHARGER");
+    case "English (US)":
+    case "Italiano":
+    case "Deutsch":
+    case "Türkçe":
+    case "Dutch":
+      return text.startsWith("DOWNLOAD");
+    case "Español":
+      return text.startsWith("DESCARGAR");
+    case "Português":
+      return text.startsWith("TRANSFERIR");
+    case "Português  (Brasil) ":
+      return text.startsWith("Carregar");
+    case "Polish":
+      return text.startsWith("POBIERZ");
+    case "Indonesian":
+      return text.startsWith("UNDUH");
+    case "Croatian":
+      return text.startsWith("PREUZMI");
+    case "Russian":
+      return text.startsWith("Закачать");
+    case "Romanian":
+      return text.startsWith("Descarcă");
+    case "繁體中文":
+    case "简体中文":
+      return text.startsWith("下載");
+    case "اللغة":
+      return text.startsWith("تحميل");
+    default:
+      return false;
+  }
 }
 
 function makeDlButton(toDl) {
